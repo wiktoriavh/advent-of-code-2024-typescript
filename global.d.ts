1908ec64c17f81b924c3cbe19ad265d3b9317df7
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2024-12-01 20:13:38 +0100

    chore: remove unnecessary files and data

--- global.d.ts
+++
@@ -1,3 +0,0 @@
-declare function solution(input: string): { part1: string; part2: string };
-
-export {};
--- src/foo.ts
+++
@@ -1,8 +0,0 @@
-import solution from "./1";
-
-console.log("Advent of Code 2024");
-
-const result = solution(`1   3
-  2   4
-  5   6`);
-console.log(result);
