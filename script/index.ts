d9b10ded21246981cca9a8895f6181c211bea23d
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2024-12-05 13:07:05 +0100

    feat: update package scripts to create new days

--- script/index.ts
+++ script/index.ts
@@ -9,7 +9,7 @@ type Solution = {
 console.log("Advent of Code 2024");
 
 const inputPath = "./inputs/{{day}}.txt";
-const solutionPath = "./{{day}}.ts";
+const solutionPath = "../src/{{day}}.ts";
 
 export async function solveDay(day: number): Promise<Solution> {
   try {
@@ -29,22 +29,36 @@ export async function solveDay(day: number): Promise<Solution> {
 
 async function runAllDays(): Promise<void> {
   try {
-    const files = await readdir(import.meta.dir);
+    const files = await readdir("./src");
     const fileNames = files
       .map((file) => Number(file.replace(extname(file), "")))
       .sort((a, b) => a - b);
 
-    fileNames.forEach(async (day) => {
-      if (Number.isNaN(day)) return;
-      const start = performance.now();
-      const result = await solveDay(day);
-      const end = performance.now();
-      console.log(`🎄 Day ${day} 🎄 \x1b[90m[${(end - start).toFixed(
-        2
-      )} ms]\x1b[0m
-      Part 1: \x1b[32m${result.part1}\x1b[0m
-      Part 2: \x1b[31m${result.part2}\x1b[0m`);
-    });
+    const days: Record<number, { part1: string; part2: string; time: number }> =
+      {};
+
+    await Promise.all(
+      fileNames.map(async (day) => {
+        if (Number.isNaN(day)) return;
+        const start = performance.now();
+        const result = await solveDay(day);
+        const end = performance.now();
+
+        days[day] = { ...result, time: end - start };
+      })
+    );
+
+    Object.keys(days)
+      .map(Number)
+      .sort((a, b) => a - b)
+      .forEach((day) => {
+        const result = days[day];
+        console.log(`🎄 Day ${day} 🎄 \x1b[90m[${result.time.toFixed(
+          2
+        )} ms]\x1b[0m
+        Part 1: \x1b[32m${result.part1}\x1b[0m
+        Part 2: \x1b[31m${result.part2}\x1b[0m`);
+      });
   } catch (error) {
     console.error("ERROR: ", error);
     throw error;
