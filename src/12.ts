42a3ea756c70e5a7c78ece63eee6fd9af14a543d
Wiktoria Van Harneveldt <w.dev+git@mailbox.org>
2024-12-12 11:09:18 +0100

    wip: d12 p1

---
+++ src/12.ts
@@ -0,0 +1,74 @@
+function solution(input: string): { part1: string; part2: string } {
+  let result1 = 0;
+  let result2 = 0;
+
+  const lines = input.split("\n").map((line) => line.split(""));
+  const regions = new Map<string, { area: Set<string>; fence: Set<string> }>();
+
+  function fillFlood(
+    x: number,
+    y: number,
+    targetPlant: string,
+    prevCoord: string
+  ): void {
+    const fence = regions.get(targetPlant)?.fence ?? new Set();
+    const area = regions.get(targetPlant)?.area ?? new Set();
+
+    if (area.has(`${x},${y}`)) {
+      return;
+    }
+
+    if (x < 0 || x >= lines[0].length || y < 0 || y >= lines.length) {
+      regions.set(targetPlant, {
+        area: area,
+        fence: fence.add(`${x},${y}:${prevCoord}`),
+      });
+      return;
+    }
+
+    const currentPlant = lines[y][x];
+    if (currentPlant !== targetPlant) {
+      regions.set(targetPlant, {
+        area: area,
+        fence: fence.add(`${x},${y}:${prevCoord}`),
+      });
+      return;
+    }
+
+    if (currentPlant === targetPlant && !area.has(`${x},${y}`)) {
+      regions.set(targetPlant, { area: area.add(`${x},${y}`), fence: fence });
+
+      fillFlood(x - 1, y, targetPlant, `${x},${y}`);
+      fillFlood(x + 1, y, targetPlant, `${x},${y}`);
+      fillFlood(x, y - 1, targetPlant, `${x},${y}`);
+      fillFlood(x, y + 1, targetPlant, `${x},${y}`);
+    }
+  }
+
+  lines.forEach((line, y) => {
+    line.forEach((plant, x) => {
+      fillFlood(x, y, plant, `${x},${y}`);
+    });
+  });
+
+  const plantTypes = regions.keys();
+
+  for (const plantType of plantTypes) {
+    const region = regions.get(plantType);
+    const area = region?.area ?? new Set();
+    const fence = region?.fence ?? new Set();
+
+    if (plantType === "C") {
+      console.log(fence);
+    }
+
+    result1 += area.size * fence.size;
+  }
+
+  return {
+    part1: result1.toString(),
+    part2: result2.toString(),
+  };
+}
+
+export default solution;
---
+++ test/12.test.ts
@@ -0,0 +1,50 @@
+import { expect, test, describe } from "bun:test";
+import solution from "../src/12";
+
+const EXAMPLE_1 = `AAAA
+BBCD
+BBCC
+EEEC`;
+const EXAMPLE_1_2 = `OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO`;
+const EXAMPLE_1_3 = `RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE`;
+const RESULT_1 = "140";
+const RESULT_1_2 = "772";
+const RESULT_1_3 = "1930";
+
+const EXAMPLE_2 = EXAMPLE_1;
+const RESULT_2 = "31";
+
+describe("Day 12", () => {
+  test("Part 1 Example 1", () => {
+    const result = solution(EXAMPLE_1);
+    expect(result.part1).toBe(RESULT_1);
+  });
+
+  test("Part 1 Example 2", () => {
+    const result = solution(EXAMPLE_1_2);
+    expect(result.part1).toBe(RESULT_1_2);
+  });
+
+  test("Part 1 Example 3", () => {
+    const result = solution(EXAMPLE_1_3);
+    expect(result.part1).toBe(RESULT_1_3);
+  });
+
+  test("Part 2", () => {
+    const result = solution(EXAMPLE_2);
+    expect(result.part2).toBe(RESULT_2);
+  });
+});
